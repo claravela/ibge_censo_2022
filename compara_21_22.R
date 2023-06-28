e3d2ce36a6ebd4e885dddbbe557b92ae99d10af1
claravela <clarinhavelasco@gmail.com>
2023-06-28 16:04:15 -0300

    versao_2

--- compara_21_22.R
+++ compara_21_22.R
@@ -15,20 +15,54 @@ glimpse(censo_22)
 
 # Eliminar linhas inúteis
 
-estima_21 <- estima_21[2:5573,]
+estima_21 <- estima_21[2:5571,]
 
 # Selecionar e renomear colunas
 
 estima_21 <- estima_21 %>%
   rename(uf = 1,
+         cod_uf = 2,
          cod_muni = 3,
          cidade_2021 = 4,
-         pop_2021 = 5) %>%
-  select(uf, cod_muni, cidade_2021, pop_2021)
+         pop_2021 = 5)
 
-# Limpeza e formatação na variável das populações
+# Criar coluna com códigos da UF + município juntos
+
+estima_21 <- estima_21 %>% 
+  mutate(cod_muni_novo = paste0(estima_21$cod_uf, estima_21$cod_muni)) %>%
+  select(uf, cod_muni_novo, cidade_2021, pop_2021)
+
+estima_21 <- estima_21 %>%
+  rename(cod_muni = 2)
+
+# Limpar e formatar variável das populações
 
 estima_21$pop_2021 <- gsub("\\([0-9]{1,2})", "", estima_21$pop_2021)
 estima_21$pop_2021 <- gsub("\\.", "", estima_21$pop_2021)
 estima_21$pop_2021 <- as.double(estima_21$pop_2021)
+estima_21$cod_muni <- as.double(estima_21$cod_muni)
 
+# Fazer o merge das duas tabelas
+
+censo_22_var <- left_join(censo_22, estima_21,
+                          by = "cod_muni")
+
+# Criar coluna com a variação entre as populações
+
+censo_22_var <- censo_22_var %>%
+  mutate(var_pop = pop_2022 - pop_2021) %>%
+  mutate(var_pop_per = var_pop * 100 / pop_2021)
+
+# Ver resumo estatístico da base de dados
+
+censo_22_var %>%
+  summarise(média = mean(var_pop_per),
+            mediana = median(var_pop_per),
+            desv_pad = sd(var_pop_per),
+            mínimo = min(var_pop_per),
+            máximo = max(var_pop_per))
+
+# Ver cidades em que a variação é maior que - 100 mil pessoas
+
+censo_22_var %>% 
+  filter(var_pop <= -100000)
